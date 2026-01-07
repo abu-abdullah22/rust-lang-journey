ef85cb49538a4abcc88762b14cb0631bfd720c83
abu-abdullah22 <rorschachmayhem@gmail.com>
2026-01-07 23:30:39 +0600

    remove target

--- borrowing_and_reference/target/debug/deps/borrowing_and_reference-03d6f6e616d47b7b.d
+++
@@ -1,5 +0,0 @@
-/Users/apple/rustHello/borrowing_and_reference/target/debug/deps/borrowing_and_reference-03d6f6e616d47b7b.d: src/main.rs
-
-/Users/apple/rustHello/borrowing_and_reference/target/debug/deps/libborrowing_and_reference-03d6f6e616d47b7b.rmeta: src/main.rs
-
-src/main.rs:
--- borrowing_and_reference/target/debug/deps/borrowing_and_reference-393d2f3e96bcf760.d
+++
@@ -1,5 +0,0 @@
-/Users/apple/rustHello/borrowing_and_reference/target/debug/deps/borrowing_and_reference-393d2f3e96bcf760.d: src/main.rs
-
-/Users/apple/rustHello/borrowing_and_reference/target/debug/deps/borrowing_and_reference-393d2f3e96bcf760: src/main.rs
-
-src/main.rs:
